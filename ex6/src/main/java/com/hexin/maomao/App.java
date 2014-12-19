e30214cf99adffa7a33f5e17a608fa51350b8eb6
MingXingTeam <813106093@qq.com>
2014-12-19 14:12:09 +0800

    1、增加SpingConfig.java的配置方式和使用方法

--- ex6/src/main/java/com/hexin/maomao/App.java
+++ ex6/src/main/java/com/hexin/maomao/App.java
@@ -8,13 +8,18 @@ public class App {
 	public static void main(String[] args) {
 		//启用Profile
 //		System.setProperty("spring.profiles.active","development");
-//		//读取spring的配置文件
+//        //读取spring的配置文件
 //		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-config-all.xml");
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class); 
+//		
+//		Person person =  (Person)ctx.getBean("person");
+//        System.out.println("Person: "+person);
 		
-		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(); 
+		ctx.getEnvironment().setActiveProfiles("development");
+		ctx.register(SpringConfig.class); 
+		ctx.refresh();
     	//获得实例对象
-        Person person =  (Person)ctx.getBean("Person.Class");
+        Person person =  (Person)ctx.getBean("person");
         System.out.println("Person: "+person);
 	}
 
